55243ff61d4d41e0dfbc761699da3bb913badf02
PatrikZh <patrik.zheji@gmail.com>
2022-12-09 08:02:05 -0800

    FIx

--- theshell1.c
+++ theshell1.c
@@ -1,5 +1,48 @@
 #include "main.h"
 /**
+ * _cd - changes directory
+ * @args: the entered arguments
+ * Return: result
+ */
+int _cd(__attribute__((unused)) char **args)
+{
+        if (args[1] ==  NULL)
+        {
+                fprintf (stderr, "ssh: expected to \"cd\"\n");
+        }
+        else if (chdir(args[1]) != 0)
+        {
+                perror("ssh");
+        }
+        return 1;
+}
+/**
+ * _exit - exit out of shell
+ * @args: the entered arguments
+ * Return: result
+ */
+int __exit(__attribute__((unused)) char **args)
+{
+        return 0;
+}
+char *builtin_str[] = {
+        "cd",
+        "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+        _cd,
+        __exit
+};
+
+/**
+ * number_bultins - function
+ * Return: result
+ */
+int number_builtins() {
+        return sizeof(builtin_str) / sizeof(char *);
+}
+/**
  * _launch - launch function
  * @args: the entered arguments
  * Return: result
